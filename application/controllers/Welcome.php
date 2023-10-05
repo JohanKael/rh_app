d8a107d667d85c386d43a2753f4d762ddc559a60
JohanKael <118510163+JohanKael@users.noreply.github.com>
2023-10-05 11:59:47 +0300

    besoin display

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,6 +6,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('header');
+		$this->load->view('besoin');
 		$this->load->view('footer');
 	}		
 }
---
+++ application/views/besoin.php
@@ -0,0 +1,73 @@
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 grid-margin stretch-card">
+				<div class="card">
+					<div class="card-body">
+						<h2 class="card-title">Insertion des besoins de l'entreprise</h2>
+						<form class="forms-sample">
+							<div class="card-body">
+								<h3>Service suppose</h3>
+								<div class="form-group">
+									<label for="horaire">Nom du service</label>
+									<input type="text" class="form-control" placeholder="Nom du service" aria-describedby="basic-addon1" name="nom_service" id="">
+								</div>
+								<div class="form-group">
+									<label for="horaire">Heure voulu</label>
+									<input type="number" class="form-control" placeholder="Heure voulu" aria-describedby="basic-addon1" name="heureVoulu" id="">
+								</div>
+								<div class="form-group">
+									<label for="horaire">Homme</label>
+									<input type="number" class="form-control" placeholder="Homme" aria-describedby="basic-addon1" name="hommeJour" id="">
+								</div>
+							</div>
+							<div class="card-body">
+								<h3>Service resultat</h3>
+								<div class="form-group">
+								<label for="diplome">Service</label>
+									<select class="form-control" id="diplome">
+										<option>Info</option>
+										<option>Compta</option>
+										<option>Communication</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<label for="horaire">Heure termine</label>
+									<input type="number" class="form-control" placeholder="Heure terminer" aria-describedby="basic-addon1" name="heureTermine" id="">
+								</div>
+							</div>
+							<div class="card-body">
+								<h3>Personnel</h3>
+								<div class="form-group">
+									<label for="diplome">Diplome</label>
+									<select class="form-control" id="diplome">
+										<option>Master</option>
+										<option>Licence</option>
+										<option>Bacc</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<label for="experience">Experience</label>
+									<select class="form-control" id="experience">
+										<option>1an</option>
+										<option>2ans</option>
+										<option>3ans</option>
+										<option>Debutant</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<label for="nationalite">Nationalite</label>
+									<select class="form-control" id="nationalite">
+										<option>Malagasy</option>
+										<option>Anglais</option>
+										<option>Francais</option>
+										<option>Autres</option>
+									</select>
+								</div>
+							</div>
+							<button type="submit" class="btn btn-primary mr-2" id="modalAnnonce">Terminer</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
