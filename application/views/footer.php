ade2cd2a3c8de8c6936409d8f1e3d5336846b353
JohanKael <118510163+JohanKael@users.noreply.github.com>
2023-10-05 10:05:41 +0300

    base display of the app : header + footer

---
+++ application/views/footer.php
@@ -0,0 +1,7 @@
+    </div>
+    
+    <script src="../../other/bootstrap/js/jquery.min.js"></script>
+    <script src="../../other/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rh-desktop-app</title>
+    <link rel="stylesheet" href="<?php echo base_url() ?>other/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>other/css/main.css">
+</head>
+
+<body>
+
+    <div class="col-md-3 navig">
+
+    </div>
+
+    <div class="col-md-9 contenu">
+    
+
+
+
+
