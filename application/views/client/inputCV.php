81a1ee44fbd0edba25143bef230a15fb74b2fe36
Vioart Vidoc Andriantsimiangy <vioartvidoc@gmail.com>
2023-10-20 08:26:08 +0300

    Update inputCV.php

--- application/views/client/inputCV.php
+++ application/views/client/inputCV.php
@@ -2,23 +2,47 @@
 <div class="col-md-8 grid-margin stretch-card">
     <h2 class="card-title">Insertion de votre CV</h2>
     <form action="" method="post">
-        <label for="">Identite : </label>
-        <div class="input-group">
-            <label for="nom">Nom</label>
-            <input type="text" class="form-control" placeholder="Nom" aria-describedby="basic-addon1" name="nom" id="nom">
+        <div class="row">
+            <div class="col-md-6">
+                <h3> <u> Identite : </u> </h3>
+                <div class="input-group">
+                    <label for="nom">Nom</label>
+                    <input type="text" class="form-control" aria-describedby="basic-addon1" name="nom" id="nom">
+                </div>
+                <div class="input-group">
+                    <label for="prenom">Prenom</label>
+                    <input type="text" class="form-control" aria-describedby="basic-addon1" name="prenom" id="prenom">
+                </div>
+                <div class="input-group">
+                    <label for="date_de_naissance">Date de naissance</label>
+                    <input type="date" class="form-control" aria-describedby="basic-addon1" name="date_de_naissance" id="date_de_naissance">
+                </div>
+                <div class="input-group">
+                    <label for="adresse">Adresse</label>
+                    <input type="number" class="form-control" aria-describedby="basic-addon1" name="adresse" id="adresse">
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <h3> <u> Contact : </u> </h3>
+                <div class="input-group">
+                    <label for="phone">Numero telephone</label>
+                    <input type="number" class="form-control" aria-describedby="basic-addon1" name="phone" id="phone">
+                </div>
+                <div class="input-group">
+                    <label for="mail">Email</label>
+                    <input type="email" class="form-control" aria-describedby="basic-addon1" name="mail" id="mail">
+                </div>
+            </div>
         </div>
-        <div class="input-group">
-            <label for="prenom">Prenom</label>
-            <input type="text" class="form-control" placeholder="Prenom" aria-describedby="basic-addon1" name="prenom" id="prenom">
+        <div class="row">
+            <h3> <u> Autres : </u> </h3>
+            <div class="input-group">
+                <label for="exp">Experience</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <input type="number" class="form-control" aria-describedby="basic-addon1" name="phone" id="phone">
+            </div>
         </div>
-        <div class="input-group">
-            <label for="date_de_naissance">Date de naissance</label>
-            <input type="date" class="form-control" placeholder="Date de naissance" aria-describedby="basic-addon1" name="date_de_naissance" id="date_de_naissance">
-        </div>
-    <br>
-        <label for="">Entrez l'heure fini : </label>
-        <input type="number" name="heure" id="" class="form-control">
-        <br>
         <input type="submit" value="Soummettre" class="btn btn-primary mr-2">
     </form>
 </div>
