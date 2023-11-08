ba98ba8a3edef5b7abc8ee92644d35082ad16e60
Marand-04 <bamasetra3@gmail.com>
2023-11-08 10:17:21 +0300

    ajout fonction get_id

--- application/models/Admin/crudModel.php
+++ application/models/Admin/crudModel.php
@@ -63,19 +63,30 @@ public function getState() {
     return $array;
 }
 
-public function getId_Element($idAttribut,$table){
-    $this->db->select($idAttribut);
-    $this->db->from($table);
-    $query = $this->db->get();
+public function get_idService($data){
+    $this->db->select('idService');
+    $this->db->where('nomservice', $data['nomservice']);
+    $this->db->where('heureTravail', $data['heureTravail']);
+    $this->db->where('hommeJour', $data['hommeJour']);
+    $query = $this->db->get('services');
     $array = array();
-    foreach($query->result_array() as $res){
-        $ar = array(
-            $idAttribut => $res[$idAttribut]
-        );
-        array_push($array,$ar);
-    }
-    return $array;
+        $row = $query->row();
+        return $row->idService; 
 }
+public function get_id($table_name, $data,$NomID) {
+    $this->db->select($NomID);
+    foreach ($data as $key => $value) {
+        $this->db->where($key, $value);
+    }
+    $query = $this->db->get($table_name);
+    $row = $query->row();
+    if ($row) {
+        return $row->$NomID;
+    } else {
+        return null; // Ou toute autre indication que l'ID n'a pas été trouvé
+    }
+}
+
 
 public function get_etat($choice){
     $this->db->select('idSitMatrimoniale');
